5f706688d3fee8200d760d825b3bd243f48a010a
Jorge Espinosa <jorgespinosa2002@gmail.com>
2025-05-08 21:30:35 -0400

    fix

--- algorithms/backwards_dawg_matching.py
+++ algorithms/backwards_dawg_matching.py
@@ -1,3 +1,6 @@
+from algorithms.dawg import dawg
+
+# el return revisar, ya que a priori no devuelve nada
 def backwards_dawg_matching(text: str, pattern: str) -> list[int]:
     """
     Searches for occurrences of 'pattern' in 'text' using the backwards DAWG matching algorithm.
@@ -8,4 +11,9 @@ def backwards_dawg_matching(text: str, pattern: str) -> list[int]:
     result = backwards_dawg_matching(text, pattern)
     print("Pattern found at positions:", result)
     """
+    # preprocesamiento
+    d = dawg(pattern)
+    # buscamos el patron
+    i = 0
+
     ...
